0bed460c13f7a1c2a86445166a041f25f8442426
Dax Raad <d@ironbay.co>
2026-04-19 20:16:25 -0400

    sync

--- packages/opencode/src/service/platform.ts
+++
@@ -1,9 +0,0 @@
-import { Context, Effect } from "effect"
-import type { PlatformInstallInput, PlatformInstallResult } from "./shared"
-import { ServiceError } from "./shared"
-
-export interface Interface {
-  readonly install: (input: PlatformInstallInput) => Effect.Effect<PlatformInstallResult, ServiceError>
-}
-
-export class Service extends Context.Service<Service, Interface>()("@opencode/ServicePlatform") {}
